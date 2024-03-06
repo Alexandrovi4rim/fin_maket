d25dbc642f5d3829fae83a97fb26b0368042c770
Alexandrovi4rim <140919828+Alexandrovi4rim@users.noreply.github.com>
2024-03-06 23:41:14 +0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import 'swiper/swiper.scss';
+import 'swiper/modules/pagination.scss';
+import '../scss/style.scss';
+import './block-2';
+import './block-3';
+import './side-bar';
+import './call';
+import './feedback';
